e5313fc9b66f9895166103530edaf150991627ec
Oscar Bareev <oscar.bareev@gmail.com>
2020-06-12 18:17:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/customlist/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.customlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Random random = new Random();
+
+    private ItemsDataAdapter adapter;
+
+    private List<Drawable> images = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        ListView listView = findViewById(R.id.listView);
+
+        fillImages();
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateRandomItemData();
+            }
+        });
+
+
+        adapter = new ItemsDataAdapter(this, null);
+        listView.setAdapter(adapter);
+
+        // При тапе по элементу списка будем показывать его данные
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                showItemData(position);
+            }
+        });
+
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                adapter.removeItem(position);
+                return true;
+            }
+        });
+    }
+
+
+    private void fillImages() {
+        images.add(getDrawable(R.drawable.ic_attach_money));
+        images.add(getDrawable(R.drawable.ic_blur_circular));
+        images.add(getDrawable(R.drawable.ic_color_lens));
+        images.add(getDrawable(R.drawable.ic_healing));
+        images.add(getDrawable(R.drawable.ic_insert_emoticon));
+    }
+
+
+    private void generateRandomItemData() {
+        adapter.addItem(new ItemData(
+                images.get(random.nextInt(images.size())),
+                "Hello" + adapter.getCount(),
+                "It\'s me",
+                random.nextBoolean()));
+    }
+
+
+    private void showItemData(int position) {
+        ItemData itemData = adapter.getItem(position);
+        Toast.makeText(MainActivity.this,
+                "Title: " + itemData.getTitle() + "\n" +
+                        "Subtitle: " + itemData.getSubtitle() + "\n" +
+                        "Checked: " + itemData.isChecked(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
